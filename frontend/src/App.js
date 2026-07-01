ab38749af8b591cd4216e2a7ebffeea700d0d8c0
Navyaasharma704 <navyaasharma13@gmail.com>
2026-07-02 03:39:39 +0530

    Connected frontend to Render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,8 @@ function App() {
     try {
 
       const response = await axios.post(
-        "http://127.0.0.1:5000/predict",
+  "https://emailspamdetection-now6.onrender.com/predict",
+  
         {
           email: email
         }
